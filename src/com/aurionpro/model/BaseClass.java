5482b20b3929cb01226dcb7227ae579ec6222838
Namrata Narkhede <78205518+namratanarkhede@users.noreply.github.com>
2024-07-23 19:23:42 +0530

    Delete src/com/aurionpro/model/BaseClass.java

--- src/com/aurionpro/model/BaseClass.java
+++
@@ -1,9 +0,0 @@
-package com.aurionpro.model;
-
-
-public class BaseClass {
-    private String privateField = "Private Field";
-    String defaultField = "Default Field";
-    protected String protectedField = "Protected Field";
-    public String publicField = "Public Field";
-}
